8b075f341e07f9ad017db2d91d1c58dd7aa53093
seekatar <seekatar@gmail.com>
2020-07-15 21:46:15 -0400

    re-add vuetify

--- vue/client/src/message-hub.ts
+++ vue/client/src/message-hub.ts
@@ -1,5 +1,5 @@
-import { HubConnectionBuilder, LogLevel } from "@aspnet/signalr"
-import axios from "axios"
+import { HubConnectionBuilder, LogLevel } from "@aspnet/signalr";
+import axios from "axios";
 
 // CORS issue w/o https
 const url = "https://localhost:5001/messagehub";
@@ -7,14 +7,17 @@ const url = "https://localhost:5001/messagehub";
 export default {
   userId: "wilma",
   install(Vue: any) {
-
-    const connection = new  HubConnectionBuilder()
+    const connection = new HubConnectionBuilder()
       .withUrl(url, {
         accessTokenFactory: async () => {
           // called before each SignalR request so can renew if needed here
           // see https://docs.microsoft.com/en-us/aspnet/core/signalr/authn-and-authz?view=aspnetcore-3.1
-          console.log(`message-hub.ts connecting with userid of ${this.userId}`);
-          const response = await axios.get(`https://localhost:5001/message/jwt?userId=${this.userId}`);
+          console.log(
+            `message-hub.ts connecting with userid of ${this.userId}`
+          );
+          const response = await axios.get(
+            `https://localhost:5001/message/jwt?userId=${this.userId}`
+          );
           return response.data;
         }
       })
@@ -26,22 +29,29 @@ export default {
     const messageHub = new Vue();
     Vue.prototype.$messageHub = messageHub;
 
-    connection.on('ReceiveMessage', (message: any) => {
-      messageHub.$emit('new-message', { ...message })
+    connection.on("ReceiveMessage", (message: any) => {
+      messageHub.$emit("new-message", { ...message });
     });
 
     // start with reconnect logic
-    let startedPromise
+    let startedPromise;
     function start() {
       startedPromise = connection.start().catch(err => {
-        console.error('Failed to connect with hub', err)
+        console.error("Failed to connect with hub", err);
         return new Promise((resolve, reject) =>
-          setTimeout(() => start().then(resolve).catch(reject), 5000))
-      })
-      return startedPromise
+          setTimeout(
+            () =>
+              start()
+                .then(resolve)
+                .catch(reject),
+            5000
+          )
+        );
+      });
+      return startedPromise;
     }
-    connection.onclose(() => start())
+    connection.onclose(() => start());
 
-    start()
+    start();
   }
-}
+};
--- vue/client/src/views/About.vue
+++ vue/client/src/views/About.vue
@@ -19,7 +19,9 @@
     <v-snackbar v-model="snackbar" :timeout="timeout" :color="color">
       {{ text }}
       <template v-slot:action="{ attrs }">
-        <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">Close</v-btn>
+        <v-btn color="pink" text v-bind="attrs" @click="snackbar = false"
+          >Close</v-btn
+        >
       </template>
     </v-snackbar>
   </div>
